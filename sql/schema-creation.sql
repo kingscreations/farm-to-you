d1a081291e068859bd339713fae6453f23bc0157
flogo1982 <florian.goussin@gmail.com>
2015-02-01 10:54:10 -0700

    sql/schema-creation.sql added

---
+++ sql/schema-creation.sql
@@ -0,0 +1,123 @@
+DROP TABLE IF EXISTS categoryProduct;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS orderProduct;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS checkout;
+DROP TABLE IF EXISTS order;
+DROP TABLE IF EXISTS storeLocation;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+	userId INT UNSIGNED AUTO_INCREMENT,
+	email VARCHAR(100),
+	hash VARCHAR(128),
+	salt VARCHAR(32),
+	activation VARCHAR(16),
+	PRIMARY KEY(userId)
+);
+
+CREATE TABLE profile (
+	profileId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	firstName VARCHAR(45),
+	lastName VARCHAR(45),
+	phone VARCHAR(20),
+	profileType VARCHAR(10),
+	customerToken VARCHAR(50),
+	imagePath VARCHAR(100),
+	userId INT UNSIGNED NOT NULL,
+	INDEX(userId),
+	FOREIGN KEY(userId) REFERENCES user(userId),
+	PRIMARY KEY(profileId)
+);
+
+CREATE TABLE store (
+	storeId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	creationDate DATETIME NOT NULL,
+	brand VARCHAR(45),
+	imagePath VARCHAR(100),
+	profileId INT UNSIGNED NOT NULL,
+	INDEX(profileId),
+	FOREIGN KEY(profileId) REFERENCES profile(profileId),
+	PRIMARY KEY(storeId)
+);
+
+CREATE TABLE location (
+	locationId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	country VARCHAR(30),
+	state CHAR(2),
+	city VARCHAR(40),
+	zipCode CHAR(5),
+	address1 VARCHAR(150),
+	address2 VARCHAR(150),
+	PRIMARY KEY(locationId)
+);
+
+CREATE TABLE storeLocation(
+	locationId INT UNSIGNED NOT NULL,
+	storeId INT UNSIGNED NOT NULL,
+	INDEX(locationId),
+	INDEX(storeId),
+	FOREIGN KEY(locationId) REFERENCES location(locationId),
+	FOREIGN KEY(storeId) REFERENCES store(storeId),
+	PRIMARY KEY(locationId, storeId)
+);
+
+CREATE TABLE order(
+	orderId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	orderDate DATETIME NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	INDEX(profileId),
+	FOREIGN KEY(profileId) REFERENCES profile(profileId),
+	PRIMARY KEY(orderId)
+);
+
+CREATE TABLE checkout (
+	checkoutId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	orderId INT UNSIGNED NOT NULL,
+	INDEX(orderId),
+	FOREIGN KEY(orderId) REFERENCES order(orderId),
+	PRIMARY KEY(checkoutId)
+);
+
+CREATE TABLE product (
+	productId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	productName VARCHAR(45) NOT NULL,
+	productPrice FLOAT(7,2) NOT NULL,
+	productType VARCHAR(40) NOT NULL,
+	productWeignt FLOAT(8, 4) NOT NULL,
+	imagePath VARCHAR(100),
+	profileId INT UNSIGNED NOT NULL,
+	INDEX(profileId),
+	FOREIGN KEY(profileId) REFERENCES profile(profileId),
+	PRIMARY KEY(productId)
+);
+
+CREATE TABLE orderProduct (
+	productQuantity INT UNSIGNED NOT NULL,
+	orderId INT UNSIGNED NOT NULL,
+	productId INT UNSIGNED NOT NULL,
+	INDEX(orderId),
+	INDEX(productId),
+	FOREIGN KEY(orderId) REFERENCES order(orderId),
+	FOREIGN KEY(productId) REFERENCES product(productId),
+	PRIMARY KEY(orderId, productId)
+);
+
+CREATE TABLE category (
+	categoryId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	categoryName VARCHAR(20) NOT NULL,
+	PRIMARY KEY(categoryId)
+);
+
+CREATE TABLE categoryProduct (
+	categoryId INT UNSIGNED NOT NULL,
+	productId INT UNSIGNED NOT NULL,
+	INDEX(categoryId),
+	INDEX(productId),
+	FOREIGN KEY(categoryId) REFERENCES category(categoryId),
+	FOREIGN KEY(productId) REFERENCES product(productId),
+	PRIMARY KEY(categoryId, productId)
+);

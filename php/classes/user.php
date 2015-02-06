582197ded40c47483df7264fa15eaf3b9229e0da
Jason King <jking109@cnm.edu>
2015-02-06 11:57:58 -0700

    Added method getAllUsers

--- php/classes/user.php
+++ php/classes/user.php
@@ -503,5 +503,55 @@ class User {
 		$statement->close();
 		return ($user);
 	}
+	/**
+	 * gets all Users
+	 *
+	 * @param resource $mysqli pointer to mySQL connection, by reference
+	 * @return mixed array of Users found or null if not found
+	 * @throws mysqli_sql_exception when mySQL related errors occur
+	 **/
+	public static function getAllUsers(&$mysqli) {
+		// handle degenerate cases
+		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
+			throw(new mysqli_sql_exception("input is not a mysqli object"));
+		}
+		// create query template
+		$query = "SELECT userId, email, hash, salt, activation FROM user";
+		$statement = $mysqli->prepare($query);
+		if($statement === false) {
+			throw(new mysqli_sql_exception("unable to prepare statement"));
+		}
+		// execute the statement
+		if($statement->execute() === false) {
+			throw(new mysqli_sql_exception("unable to execute mySQL statement: " . $statement->error));
+		}
+		// get result from the SELECT query
+		$result = $statement->get_result();
+		if($result === false) {
+			throw(new mysqli_sql_exception("unable to get result set"));
+		}
+		// build an array of users
+		$users = array();
+		while(($row = $result->fetch_assoc()) !== null) {
+			try {
+				$user	= new User($row["userId"], $row["email"], $row["hash"], $row["salt"], $row["activation"]);
+				$users[] = $user;
+			}
+			catch(Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new mysqli_sql_exception($exception->getMessage(), 0, $exception));
+			}
+		}
+		// count the results in the array and return:
+		// 1) null if 0 results
+		// 2) the entire array if >= 1 result
+		$numberOfUsers = count($users);
+		if($numberOfUsers === 0) {
+			return(null);
+		} else {
+			return($users);
+		}
+	}
+
 }
 ?>

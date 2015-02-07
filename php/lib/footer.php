da1bd20cf147d7d5f5f3390bf19729f27fec5dc7
flogo1982 <florian.goussin@gmail.com>
2015-02-07 14:21:29 -0700

    footer: done header: drop-down colors

--- php/lib/footer.php
+++ php/lib/footer.php
@@ -6,6 +6,30 @@
  */
 ?>
 
+			<footer>
+				<div class="row-fluid">
+					<div class="col-sm-3">
+						<p>
+							<a href="">Jason</a>
+						</p>
+					</div>
+					<div class="col-sm-3">
+						<p>
+							<a href="">Jay</a>
+						</p>
+					</div>
+					<div class="col-sm-3">
+						<p>
+							<a href="">Alonso</a>
+						</p>
+					</div>
+					<div class="col-sm-3">
+						<p>
+							<a href="">Florian</a>
+						</p>
+					</div>
+				</div>
+			</footer>
 		</div><!-- end of container-fluid -->
 	</body>
 </html>

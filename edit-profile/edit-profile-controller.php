8ed6949a80b1ff795c8df001240c42515177faa6
Jay Renteria <jrenteria13@cnm.edu>
2015-02-09 17:10:39 -0700

    updated css for form controller margin, added controller and form for edit-profile

---
+++ edit-profile/edit-profile-controller.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../php/classes/profile.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+
+// verify the form values have been submitted
+if(@isset($_POST["InputFirstname"]) === false || @isset($_POST["InputLastname"]) === false || @isset($_POST["InputEmail"]) === false
+			|| @isset($_POST["InputType"]) === false || @isset($_POST["InputPhone"]) === false || @isset($_POST["InputImage"]))  {
+	echo "<p class=\"alert alert-danger\">Form values not complete. Verify the form and try again.</p>";
+}
+
+try {
+	//
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
+	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+	$profile = new Profile(null, $_POST["InputFirstname"], $_POST["InputLastname"], $_POST["InputPhone"], $_POST["InputType"], "012345", $_POST["InputImage"], null);
+	$profile->insert($mysqli);
+	echo "<p class=\"alert alert-success\">Profile (id = " . $profile->getProfileId() . ") posted!</p>";
+} catch(Exception $exception) {
+	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+}
+
---
+++ php/forms/edit-profile-form.php
@@ -0,0 +1,60 @@
+<?php require_once("../lib/header.php"); ?>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/2.7.5/idangerous.swiper.min.css"/>
+<link rel="stylesheet" href="../../css/main.css"/>
+
+<div class="container">
+	<h2>Edit Profile</h2>
+
+	<form class="form-inline" method="post" action="../../edit-profile/edit-profile-controller.php">
+
+		<div class="form-group">
+			<label for="InputFirstname">First Name:</label>
+			<input type="text" class="form-control" id="InputFirstname" name="InputFirstname" placeholder="Enter First Name">
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<label for="InputLastname">Last Name:</label>
+			<input type="text" class="form-control" id="InputLastname" name="InputLastname" placeholder="Enter Last Name">
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<label for="InputEmail">Email Address:</label>
+			<input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter email">
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<label for="InputType">Profile Type:</label>
+			<input type="radio" class="form-control" name="InputType" id="InputType" value="m">Merchant
+			<input type="radio" class="form-control" name="InputType" id="InputType" value="c">Client
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<label for="InputPhone">Phone Number:</label>
+			<input type="tel" class="form-control" i	d="InputPhone" name="InputPhone" placeholder="Enter Phone Number">
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<label for="InputImage">Profile Image</label>
+			<input type="file" class="form-control" id="InputImage" name="InputImage">
+		</div>
+
+		<br>
+
+		<div class="form-group">
+			<input type="submit" class="form-control" id="InputSubmit" name="InputSubmit">
+		</div>
+
+	</form>
+</div>

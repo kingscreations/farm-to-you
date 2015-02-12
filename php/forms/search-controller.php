54d23b69e9b0196831fc38f03f2c5cc5c38494dc
Jay Renteria <jrenteria13@cnm.edu>
2015-02-12 15:30:29 -0700

    search working on a scaled down level

--- php/forms/search-controller.php
+++ php/forms/search-controller.php
@@ -1,9 +1,51 @@
 <?php
 require_once("../classes/product.php");
+require_once("../classes/store.php");
 require_once("../classes/category.php");
+require_once("../classes/location.php");
 require_once("../classes/categoryproduct.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-if(@isset($_POST["inputSubmit"]) && @isset($_POST["inputSearch"]) != "") {
+
+if(@isset($_POST["inputSubmit"]) && (@isset($_POST["inputSearch"]) != "")) {
 	echo "<p class=\"alert alert-danger\">Form values not complete. Verify the form and try again.</p>";
 }
+
+
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
+	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+
+	$searchq = $_POST["inputSearch"];
+	$searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
+
+// query the database. The amount of columns have to match as it currently is. Need more from product and location though
+	$result = mysqli_query($mysqli, "SELECT productName, productPrice FROM product WHERE productName LIKE '%$searchq%' OR productDescription LIKE '%$searchq%'
+											UNION
+												SELECT storeName, imagePath  FROM store WHERE storeName LIKE '%$searchq%'
+													UNION
+														SELECT locationName, address1 FROM location WHERE locationName LIKE '%$searchq%'");
+
+if (!$result) {
+	printf("Error: %s\n", mysqli_error($mysqli));
+	exit();
+}
+
+print_r(mysqli_fetch_array($result));
+while($row = mysqli_fetch_array($result)){
+	echo "{$row['productName']}";
+}
+
+
+var_dump($result);
+//	print '<table border="1">';
+//	while($row = $results->fetch_assoc()) {
+//		print '<tr>';
+//		print '<td>'.$row["id"].'</td>';
+//		print '<td>'.$row["product_code"].'</td>';
+//		print '<td>'.$row["product_name"].'</td>';
+//		print '<td>'.$row["product_desc"].'</td>';
+//		print '<td>'.$row["price"].'</td>';
+//		print '</tr>';
+//	}
+//	print '</table>';

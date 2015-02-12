15eae2afb83eaa11c8987a5193570c289b85cb05
Jay Renteria <jrenteria13@cnm.edu>
2015-02-12 15:31:36 -0700

    added comments

--- php/forms/search-controller.php
+++ php/forms/search-controller.php
@@ -6,12 +6,12 @@ require_once("../classes/location.php");
 require_once("../classes/categoryproduct.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-
+// check if search was entered or not
 if(@isset($_POST["inputSubmit"]) && (@isset($_POST["inputSearch"]) != "")) {
 	echo "<p class=\"alert alert-danger\">Form values not complete. Verify the form and try again.</p>";
 }
 
-
+// connect to database and filter search
 	mysqli_report(MYSQLI_REPORT_STRICT);
 	$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
@@ -26,17 +26,19 @@ if(@isset($_POST["inputSubmit"]) && (@isset($_POST["inputSearch"]) != "")) {
 													UNION
 														SELECT locationName, address1 FROM location WHERE locationName LIKE '%$searchq%'");
 
+// check for errors in the search
 if (!$result) {
 	printf("Error: %s\n", mysqli_error($mysqli));
 	exit();
 }
 
+// print results
 print_r(mysqli_fetch_array($result));
 while($row = mysqli_fetch_array($result)){
 	echo "{$row['productName']}";
 }
 
-
+// try to print a table
 var_dump($result);
 //	print '<table border="1">';
 //	while($row = $results->fetch_assoc()) {

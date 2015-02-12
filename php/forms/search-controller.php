a6f0021fbc1c44aef6749344ad07ac3c33425b94
Jay Renteria <jrenteria13@cnm.edu>
2015-02-12 16:17:51 -0700

    tables separated

--- php/forms/search-controller.php
+++ php/forms/search-controller.php
@@ -20,14 +20,20 @@ if(@isset($_POST["inputSearch"]) === false) {
 	$searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
 
 // query the database. The amount of columns have to match as it currently is. Need more from product and location though
-	$result = mysqli_query($mysqli, "SELECT productName, productPrice, productDescription FROM product WHERE productName LIKE '%$searchq%' OR productDescription LIKE '%$searchq%'
-											UNION
-												SELECT storeName, imagePath, storeDescription  FROM store WHERE storeName LIKE '%$searchq%'
-													UNION
-														SELECT locationName, address1, city FROM location WHERE locationName LIKE '%$searchq%'");
+	$result1 = mysqli_query($mysqli, "SELECT productName, productPrice, productDescription FROM product WHERE productName LIKE '%$searchq%' OR productDescription LIKE '%$searchq%'");
+	$result2 = mysqli_query($mysqli,"SELECT storeName, imagePath, storeDescription  FROM store WHERE storeName LIKE '%$searchq%'");
+	$result3 = mysqli_query($mysqli, "SELECT locationName, address1, city FROM location WHERE locationName LIKE '%$searchq%'");
 
 // check for errors in the search
-if (!$result) {
+if (!$result1) {
+	printf("Error: %s\n", mysqli_error($mysqli));
+	exit();
+}
+if (!$result2) {
+	printf("Error: %s\n", mysqli_error($mysqli));
+	exit();
+}
+if (!$result3) {
 	printf("Error: %s\n", mysqli_error($mysqli));
 	exit();
 }
@@ -40,7 +46,7 @@ if (!$result) {
 
 // try to print a table
 	print '<table border="1">';
-	while($row = mysqli_fetch_array($result)) {
+	while($row = mysqli_fetch_array($result1)) {
 		print '<tr>';
 		print '<th>Product</th>';
 		print '<th>Description</th>';
@@ -49,9 +55,42 @@ if (!$result) {
 		print '<tr>';
 //		print '<td>'.$row["id"].'</td>';
 //		print '<td>'.$row["product_code"].'</td>';
-		print '<td>'.$row["productName"].'</td>';
-		print '<td>'.$row["productDescription"].'</td>';
-		print '<td>'.$row["productPrice"].'</td>';
+		print '<td>' . $row["productName"] . '</td>';
+		print '<td>' . $row["productDescription"] . '</td>';
+		print '<td>' . $row["productPrice"] . '</td>';
 		print '</tr>';
 	}
+print '</table>';
+
+	print '<table border="1">';
+	while($row = mysqli_fetch_array($result2)) {
+		print '<tr>';
+		print '<th>Store</th>';
+		print '<th>Image</th>';
+		print '<th>Description</th>';
+		print '</tr>';
+		print '<tr>';
+//		print '<td>'.$row["id"].'</td>';
+//		print '<td>'.$row["product_code"].'</td>';
+		print '<td>' . $row["storeName"] . '</td>';
+		print '<td>' . $row["imagePath"] . '</td>';
+		print '<td>' . $row["storeDescription"] . '</td>';
+	}
 	print '</table>';
+
+print '<table border="1">';
+while($row = mysqli_fetch_array($result3)) {
+	print '<tr>';
+	print '<th>Location</th>';
+	print '<th>Address</th>';
+	print '<th>City</th>';
+	print '</tr>';
+	print '<tr>';
+//		print '<td>'.$row["id"].'</td>';
+//		print '<td>'.$row["product_code"].'</td>';
+	print '<td>' . $row["locationName"] . '</td>';
+	print '<td>' . $row["address1"] . '</td>';
+	print '<td>' . $row["city"] . '</td>';
+	print '</tr>';
+}
+print '</table>';

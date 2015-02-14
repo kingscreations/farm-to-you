d4cd1c500cc4311c04f8027a44a5c09c17cd0f1c
flogo1982 <florian.goussin@gmail.com>
2015-02-13 20:16:23 -0700

    doing the total price only with js/jQuery

--- js/cart.js
+++ js/cart.js
@@ -39,22 +39,6 @@
 		 if($(this) === null || $(this).length === 0) {
 			 return;
 		 }
-		 //console.log($(this));
-		 //console.log($(this).parents());
-
-		 // get the entire row
-		 $.each($(this).parents(), function() {
-			console.log($(this).prop('tagName'))
-			 if($(this).prop('tagName') === 'TR') {
-
-				 $.each($(this).find('td'), function() {
-					console.log($(this));
-				 });
-
-				 console.log($(this).find('td'));
-				 return false; // you have to return explicitly false to exit the loop
-			 }
-		 })
 
 		 // first step to be able to get the other cell of this product row
 		 var elementId = $(this)[0].id;
@@ -65,30 +49,40 @@
 		 var $inputPrice = $('#'+ elementIdPart1 +'-price');
 		 var $inputWeight = $('#'+ elementIdPart1 +'-weight');
 
-		 // get the price and the product price type
+		 var productWeight = parseFloat($inputWeight.text());
 		 var productPrice = null;
-		 if($inputPrice.val().indexOf('/lb') >= 0) {
-			 var productPriceType = 'w';
-			 productPrice = $inputPrice.split('/lb');
+		 var newQuantity = parseFloat($(this).val());
+
+		 console.log(productWeight);
+		 console.log(newQuantity);
+
+		 // set the total price according to the productPriceType
+		 if($inputPrice.text().indexOf('lb') >= 0) {
+			 productPrice = parseFloat($inputPrice.text().split('/lb')[0].substring(1));
+
+			 var finalPrice = String(Math.round(productPrice * newQuantity * productWeight), 2);
+			 $('#'+ elementIdPart1 +'-total-price').html('$'+finalPrice);
 		 } else {
-			 var productPriceType = 'u';
-			 productPrice = $inputPrice.val();
+			 productPrice = parseFloat($inputPrice.text()[0].substring(1));
+
+			 var finalPrice = String(Math.round(productPrice * newQuantity), 2);
+			 $('#'+ elementIdPart1 +'-total-price').html('$'+finalPrice);
 		 };
 
-		 var productWeight = $inputWeight.val();
-		//console.log($inputWeight);
-		var data = {
-			'newQuantity': $(this).val(),
-			'productPrice': productPrice,
-			'productPriceType': productPriceType,
-			'productWeight': productWeight
-		}
-		$.ajax({
-			 type: 'post',
-			 url: '../php/forms/cart-controller.php',
-			 data: data
-		}).done(function(ajaxOutput) {
-			$('#'+ elementIdPart1 +'-total-price').html(ajaxOutput);
-		});
+		 //console.log(productPrice);
+		 //console.log($inputWeight);
+		//var data = {
+		//	'newQuantity': $(this).val(),
+		//	'productPrice': productPrice,
+		//	'productPriceType': productPriceType,
+		//	'productWeight': productWeight
+		//}
+		//$.ajax({
+		//	 type: 'post',
+		//	 url: '../php/forms/cart-controller.php',
+		//	 data: data
+		//}).done(function(ajaxOutput) {
+		//	$('#'+ elementIdPart1 +'-total-price').html(ajaxOutput);
+		//});
 	 })
 });

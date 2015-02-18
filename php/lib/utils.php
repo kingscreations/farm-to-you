9e83bc2e8bc304a6959ed04636e4992ff110e64b
flogo1982 <florian.goussin@gmail.com>
2015-02-18 09:41:11 -0700

    check input function: add a doc string

--- php/lib/utils.php
+++ php/lib/utils.php
@@ -1,11 +1,20 @@
 <?php
 
+/**
+ * @param $inputImage the input image. e.g.: $_FILES['image']
+ * @return string the extension of the image
+ *
+ * @throw InvalidArgumentException if the input png file is not valid
+ * @throw InvalidArgumentException if the input jpg file is not valid
+ * @throw RangeException if the type is different than jpg, JPG, jpeg, JPEG, png or PNG
+ * @throw RangeException if the mime type is different than jpg or png
+ */
 function checkInputImage($inputImage) {
 	// check extension for normal users
 	$extensions = array("jpg", "jpeg", "png");
 	$extension  = strtolower(end(explode(".", $inputImage["name"])));
 	if(in_array($extension, $extensions) === false) {
-		throw new InvalidArgumentException('The input image file is not valid');
+		throw new RangeException('The input image file should be either jpg, JPG, jpeg, JPEG, png or PNG');
 	}
 
 	// check file content for malicious users and totally incompetent users

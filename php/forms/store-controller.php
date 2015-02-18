795a21ee6d612cbc0e3299292181e4c8bc2d14ae
flogo1982 <florian.goussin@gmail.com>
2015-02-18 09:02:56 -0700

    check image file function wip

--- php/forms/store-controller.php
+++ php/forms/store-controller.php
@@ -1,19 +1,46 @@
 <?php
 session_start();
-var_dump($_POST);
-var_dump($_FILES);
-$imageinfo = getimagesize($_FILES["inputImage"]['tmp_name']);
-
-$imageWidth  = $imageinfo[0];
-$imageHeight = $imageinfo[1];
-$imageFile = @imagecreatetruecolor($imageWidth, $imageHeight);
 
 $imageBasePath = '/var/www/html/farmtoyou/images/store';
-// TODO change this blueprint by including the store id in the middle and adding the file extension according to the mime type
 $imageFileName = 'store-$storeId.{jpg|png}';
 
-move_uploaded_file($imageFile, 'path');
-var_dump($imageinfo);
+
+function checkInputImage($inputImage) {
+	// check extension for normal users
+	$extensions = array("jpg", "jpeg", "png");
+	$extension  = strtolower(end(explode(".", $inputImage["name"])));
+	if(in_array($extension, $extensions) === false) {
+		echo "this is not a valid file";
+		return false;
+	}
+
+	// check file content for malicious users and totally incompetent users
+	$mimeType = $inputImage["type"];
+	if($mimeType !== 'image/png' || $mimeType !== 'image/jpeg') {
+		echo 'Sorry, we only accept GIF and JPEG images\n';
+		return false;
+	}
+
+	if($mimeType === "image/png") {
+		if(($image = @imagecreatefrompng($inputImage["tmp_name"])) === false) {
+			throw new InvalidArgumentException('The input png image format is incorrect');
+		}
+	}
+
+	if($mimeType === "image/jpg") {
+		if(($image = @imagecreatefromjpeg($inputImage["tmp_name"])) === false) {
+			throw new InvalidArgumentException('The input jpg image format is incorrect');
+		}
+	}
+
+	var_dump($image);
+
+	// want to resize/crop/vandalize Alonso's images?
+	// do so here!
+//	imagedestroy($image);
+
+	return true;
+}
 
 $currentDir = dirname(__FILE__);
 require_once("../../dummy-session.php");

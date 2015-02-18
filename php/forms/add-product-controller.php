be44cbe4cbcbf1520a7de542aad0974672dea51e
Jay Renteria <jrenteria13@cnm.edu>
2015-02-18 11:35:26 -0700

    sessions working with add product

--- php/forms/add-product-controller.php
+++ php/forms/add-product-controller.php
@@ -17,16 +17,7 @@ if(@isset($_POST["inputProductName"]) === false || @isset($_POST["inputProductPr
 	echo "<p class=\"alert alert-danger\">Form values not complete. Verify the form and try again.</p>";
 }
 
-function getRandomWord($len = 10) {
-	$word = array_merge(range('a', 'z'), range('A', 'Z'));
-	shuffle($word);
-	return substr(implode($word), 0, $len);
-}
-$randActivation = bin2hex(openssl_random_pseudo_bytes(8));
-
-$randSalt = bin2hex(openssl_random_pseudo_bytes(16));
-
-$randHash = bin2hex(openssl_random_pseudo_bytes(64));
+$profileId = $_SESSION['profile']['id'];
 
 try {
 	//
@@ -34,18 +25,25 @@ try {
 	$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 
-	$user = new User(null, getRandomWord() . "@test.com", $randHash, $randSalt, $randActivation);
-	$user->insert($mysqli);
-	$profile = new Profile(null, "Test19", "Test2", "5555555555", "m", "012345", "http://www.cats.com/cat.jpg", $user->getUserId());
-	$profile->insert($mysqli);
 
 	if(@isset($_POST["inputProductImage"])) {
-		$product = new Product(null, $profile->getProfileId(), $_POST["inputProductImage"], $_POST["inputProductName"], $_POST["inputProductPrice"], $_POST["inputProductDescription"], $_POST["inputProductPriceType"], $_POST["inputProductWeight"], $_POST["inputStockLimit"]);
+		$product = new Product(null, $profileId, $_POST["inputProductImage"], $_POST["inputProductName"], $_POST["inputProductPrice"], $_POST["inputProductDescription"], $_POST["inputProductPriceType"], $_POST["inputProductWeight"], $_POST["inputStockLimit"]);
 	} else {
-		$product = new Product(null, $profile->getProfileId(), null, $_POST["inputProductName"], $_POST["inputProductPrice"], $_POST["inputProductDescription"], $_POST["inputProductPriceType"], $_POST["inputProductWeight"], $_POST["inputStockLimit"]);
+		$product = new Product(null, $profileId, null, $_POST["inputProductName"], $_POST["inputProductPrice"], $_POST["inputProductDescription"], $_POST["inputProductPriceType"], $_POST["inputProductWeight"], $_POST["inputStockLimit"]);
 	}
 
 	$product->insert($mysqli);
+
+	$_SESSION['product'] = array(
+		'id' 				=> $product->getProductId(),
+		'name'			=> $product->getProductName(),
+		'price'	=> $product->getProductPrice(),
+		'image'			=> $product->getImagePath(),
+		'description'		=> $product->getProductDescription(),
+		'weight' => $product->getProductWeight(),
+		'stock' => $product->getStockLimit(),
+		'priceType' => $product->getProductPriceType()
+	);
 
 	echo "<p class=\"alert alert-success\">Product (id = " . $product->getProductId() . ") posted!</p>";
 } catch(Exception $exception) {

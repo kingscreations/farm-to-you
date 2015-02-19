f94f121bda74e280fa444edd70cc06420ba83d25
flogo1982 <florian.goussin@gmail.com>
2015-02-19 09:46:23 -0700

    cart test pass

---
+++ test/funcunit/ftest-cart.js
@@ -0,0 +1,29 @@
+// open a new window with the form under scrutiny
+module("tabs", {
+	setup: function() {
+		F.open("../../cart/");
+	}
+});
+
+var VALID_QUANTITY      = "12";
+
+var $cartQuantities = $('table.table tbody tr [id$=-quantity]');
+
+/**
+ * test filling in only valid form data
+ **/
+function testValidFields() {
+	// fill in the form values\
+	$cartQuantities.val(12);
+
+	// click the button once all the fields are filled in
+	F("#cart-validate-button").click();
+
+	F.wait(2000, function(){
+		ok(F('h2').text() === 'You don\'t have any choice for the pickup location even if you are supposed to',
+			'on the pickup location page')
+	})
+}
+
+// start the test
+test("test valid fields", testValidFields);

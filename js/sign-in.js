a7e0d6b178677792c23e5078421b7390ee181f91
Jason King <jking109@cnm.edu>
2015-02-19 15:56:41 -0700

    created sign-in js

---
+++ js/sign-in.js
@@ -0,0 +1,64 @@
+/**
+ * Created by jason on 2/19/2015.
+ */
+// document ready event
+$(document).ready(
+// inner function for the ready() event
+	function() {
+// tell the validator to validate this form
+		$("#signIn").validate({
+// setup the formatting for the errors
+			errorClass: "label-danger",
+			errorLabelContainer: "#outputArea",
+			wrapper: "li",
+// rules define what is good/bad input
+			rules: {
+// each rule starts with the inputs name (NOT id)
+				email: {
+					maxlength: 140,
+					required: true,
+					email: true
+				},
+				password: {
+					maxlength: 200,
+					required: true
+				},
+			},
+			// error messages to display to the end user
+			messages: {
+				email: {
+					min: "Email is too long",
+					required: "Please enter an email address",
+					email: "Please enter a valid email address"
+				},
+				password: {
+					maxlength: "Password is too long.",
+					required: "please enter a password."
+				},
+			},
+
+			// setup an AJAX call to submit the form without reloading
+			submitHandler: function(form) {
+				$(form).ajaxSubmit({
+					// GET or POST
+					type: "POST",
+					// where to submit data
+					url: "../php/forms/sign-in-controller.php",
+					// TL; DR: reformat POST data
+					data: $(form),
+					// success is an event that happens when the server replies
+					success: function(ajaxOutput) {
+						// clear the output area's formatting
+						$("#outputArea").css("display", "");
+						// write the server's reply to the output area
+						$("#outputArea").html(ajaxOutput);
+						// reset the form if it was successful
+						// this makes it easier to reuse the form again
+						if($(".alert-success").length >= 1) {
+							$(form)[0].reset();
+						}
+					}
+				});
+			}
+		});
+	});

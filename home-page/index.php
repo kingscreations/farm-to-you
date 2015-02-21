7ba3e5a76c67b35af074167c5754beb86ba23930
Jason King <jking109@cnm.edu>
2015-02-21 13:05:08 -0700

    Created activation-controller Started home-page

--- home-page/index.php
+++ home-page/index.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: jason
  * Date: 2/21/2015
- * Time: 12:22 PM
+ * Time: 12:59 PM
  */
 $currentDir = dirname(__FILE__);
 require_once '../root-path.php';
@@ -12,18 +12,8 @@ require_once("../php/lib/header.php");
 
 <div class="row-fluid">
 	<div class=""col-sm-12">
-		<h3>Your registration is complete. </br> Thank you for signing up with farm to you!</h3>
+	<h3>Welcome Back!</h3>
 
-// link to take user to home page
-<p>
-	<ul>
-		<li><a href="../../home-page/index.php">Continue to farm-to-you home page</a>
-
-</li>
-</ul>
-</p>
-
-	</div>
 
 </div><!-- end row-fluid -->
 <?php
---
+++ php/forms/activation-controller.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 2/21/2015
+ * Time: 12:38 PM
+ */
+require_once("../classes/user.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+// require CSRF protection
+require_once("../lib/csrf.php");
+
+start_session();
+
+// pull the user from the database
+$user = getUserBySomething();
+
+// populate the session with users info
+$_SESSION['user'] = array(
+	'id' => $user->getUserId()
+);
+
+

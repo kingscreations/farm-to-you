9609e3894377df3f29cad4b030b2351a903659a8
Jay Renteria <jrenteria13@cnm.edu>
2015-02-23 09:56:59 -0700

    added image path

--- php/forms/add-profile-controller.php
+++ php/forms/add-profile-controller.php
@@ -8,6 +8,7 @@ require_once("../classes/profile.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../classes/user.php");
 require_once("../lib/footer.php");
+require_once("../lib/utils.php");
 
 
 
@@ -31,18 +32,20 @@ try {
 		$profile = new Profile(null, $_POST["inputFirstname"], $_POST["inputLastname"], $_POST["inputPhone"], $_POST["inputType"], "012345", null, $user->getUserId());
 	}
 
-//	if(empty($_FILES['inputImage']) === false) {
-//		$imageExtension = checkInputImage($_FILES['inputImage']);
-//		$profile->insert($mysqli);
-//		$profileId = $profile->getProfileId();
-//		$imageFileName = 'profile' . $profileId . '.' . $imageExtension;
-//		$profile->setImagePath($imageFileName);
-//		$profile->update($mysqli);
-//	} else {
-//		$profile->insert($mysqli);
-//		$profileId = $profile->getProfileId();
-//	}
-	$profile->insert($mysqli);
+	if(@isset($_FILES['inputImage'])) {
+		$imageBasePath = '/var/www/html/farm-to-you/images/profile/';
+		$imageExtension = checkInputImage($_FILES['inputImage']);
+		$profile->insert($mysqli);
+		$profileId = $profile->getProfileId();
+		$imageFileName = $imageBasePath . 'profile-' . $profileId . '.' . $imageExtension;
+		$profile->setImagePath($imageFileName);
+		$profile->update($mysqli);
+		move_uploaded_file($_FILES['inputImage']['tmp_name'], $imageFileName);
+	} else {
+		$profile->setImagePath(null);
+		$profile->insert($mysqli);
+		$profileId = $profile->getProfileId();
+	}
 
 	echo "<p class=\"alert alert-success\">Profile (id = " . $profile->getProfileId() . ") posted!</p>";
 } catch(Exception $exception) {
--- php/forms/edit-profile-controller.php
+++ php/forms/edit-profile-controller.php
@@ -5,6 +5,7 @@ require_once ("../../root-path.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../../dummy-session-single.php");
 require_once("../classes/profile.php");
+require_once("../lib/utils.php");
 
 
 // verify the form values have been submitted
@@ -31,7 +32,22 @@ try {
 		$profile = new Profile($profileId, $_POST["inputFirstname"], $_POST["inputLastname"], $_POST["inputPhone"], $_POST["inputType"], "012345", null, $userId);
 
 	}
-	$profile->update($mysqli);
+
+	if(@isset($_FILES['inputImage'])) {
+		$imageBasePath = '/var/www/html/farm-to-you/images/profile/';
+		$imageExtension = checkInputImage($_FILES['inputImage']);
+		$profile->insert($mysqli);
+		$profileId = $profile->getProfileId();
+		$imageFileName = $imageBasePath . 'profile-' . $profileId . '.' . $imageExtension;
+		$profile->setImagePath($imageFileName);
+		$profile->update($mysqli);
+		move_uploaded_file($_FILES['inputImage']['tmp_name'], $imageFileName);
+	} else {
+		$profile->setImagePath(null);
+		$profile->update($mysqli);
+		$profileId = $profile->getProfileId();
+	}
+
 	echo "<p class=\"alert alert-success\">Profile (id = " . $profile->getProfileId() . ") updated!</p>";
 } catch(Exception $exception) {
 	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";

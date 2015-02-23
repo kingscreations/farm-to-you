7afa53405cfbec3f9b530d8463cefd01392daf86
Jason King <jking109@cnm.edu>
2015-02-23 09:25:17 -0700

    tuning the sign-in-controller

--- php/forms/sign-in-controller.php
+++ php/forms/sign-in-controller.php
@@ -9,7 +9,7 @@
 require_once("../classes/user.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 // require CSRF protection
-// require_once("../lib/csrf.php");
+ require_once("../lib/csrf.php");
 
 // CSRF requires sessions
 session_start();
@@ -20,32 +20,40 @@ try {
 	}
 
 	// verify the CSRF tokens
-//	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
-//		throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled."));
-//	}
+	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
+		throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled."));
+	}
+
+	// filter _POST variables
+$email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+$password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
 
-//$_SESSION['user'] = array(
-//	'id' => $user->getUserId()
-//);
 
+// gain access to database
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
+	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 
-		// get the users email from mysqli
-	$mysqlUser = User::getUserByEmail($this->mysqli, $this->email);
-	$this->assertIdentical($this->user->email(), $mysqlUser->getEmail());
+	// get the users email from mysqli
+	$mysqlEmail = User::getUserByEmail($mysqli, $email);
 
-	$user = getUserByEmail();
+	//get the mysqli hash and salt
+	$mysqlHash = User($mysqli, $hash);
+	$salt = User($mysqli, $salt);
 
-	// generate hash and retrieve salt
-	$salt = $this->mysqlUser->getUserSalt;
+	// generate hash from users password using mysqli salt
 	$hash = hash_pbkdf2("sha512", "password", $salt, 2048, 128);
 
-	// retrieve users hash and compare to input
-	$userHash = $this->mysqlUser->getUserHash;
-	if ($userHash !== $hash) {
+	// compare hashes
+	if ($mysqlHash !== $hash) {
 		throw new Exception('email input does not match existing account');
 	}
 	// catch any exceptions
 } catch(Exception $exception) {
 	echo "<p class=\"input not posted!\">Exception: " . $exception->getMessage() . "</p>";
 }
+// create session id specific to this user
+$_SESSION['user'] = array(
+	'id' => $user->getUserId()
+);

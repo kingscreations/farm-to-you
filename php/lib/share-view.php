94ca7b6d7ef8218736154ade12f80c31274f5d04
flogo1982 <florian.goussin@gmail.com>
2015-02-28 21:44:11 -0700

    align share buttons

--- php/lib/share-view.php
+++ php/lib/share-view.php
@@ -1,13 +1,17 @@
 <!-- facebook share button -->
-<div class="fb-share-button"
-	  data-href="https://developers.facebook.com/docs/plugins/"
-	  data-layout="button">
+<div id="facebook-share-button" class="share-button">
+	<div class="fb-share-button"
+		  data-href="https://developers.facebook.com/docs/plugins/"
+		  data-layout="button">
+	</div>
 </div>
 
 <!-- twitter share button -->
-<a class="twitter-share-button" href="https://twitter.com/share"
-	data-related="twitterdev"
-	data-size="large"
-	data-count="none">
-	Tweet
-</a>
+<div id="twitter-share-button" class="share-button">
+	<a class="twitter-share-button" href="https://twitter.com/share"
+		data-related="twitterdev"
+		data-size="large"
+		data-count="none">
+		Tweet
+	</a>
+</div>

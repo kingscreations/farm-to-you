ba85724d7ca1948985f90ff4292a6105f4bcd921
flogo1982 <florian.goussin@gmail.com>
2015-03-01 22:30:22 -0700

    mobile version of the splash page

--- new-user/index.php
+++ new-user/index.php
@@ -7,7 +7,7 @@ require_once('../php/lib/header.php');
 ?>
 
 <div class="home-top">
-	<div class="home-top-search-area">
+	<div class="home-top-search-area hidden-xs">
 		<h1 class="heading">Delicious products and fair trades directly from the farmers</h1>
 		<form class="mt30" action="../php/forms/search-controller.php" id="search" method="post">
 			<div class="input-group">
@@ -15,11 +15,32 @@ require_once('../php/lib/header.php');
 				<span class="input-group-btn">
 				  <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
 				</span>
-			</div><!-- end input-group -->
+			</div>
 		</form>
 		<p class="outputArea"></p>
 	</div>
+
+	<div class="container-fluid visible-xs">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="heading">Delicious products and fair trades directly from the farmers</h1>
+				<form class="mt30" action="../php/forms/search-controller.php" id="search" method="post">
+					<div class="input-group">
+						<input class="form-control search-field" type="text" id="inputSearch" name="inputSearch" placeholder="What are looking for today?" />
+						<span class="input-group-btn">
+						  <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+						</span>
+					</div>
+				</form>
+				<p class="outputArea"></p>
+			</div>
+		</div>
+	</div>
+
 </div>
+
+
+
 <div class="home-main">
 
 </div>
---
+++ search/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// search result view
+
+// header
+$currentDir = dirname(__FILE__);
+require_once('../root-path.php');
+require_once('../php/lib/header.php');
+
+
+?>
+
+<?php require_once('../php/lib/footer.php');

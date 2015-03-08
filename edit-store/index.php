e4244837c7571c71c372afd58d41174e96cd1349
Alonso Indacochea <aindaco1@hotmail.com>
2015-03-08 15:24:38 -0600

    changed media query for .user-account submit

--- edit-store/index.php
+++ edit-store/index.php
@@ -45,52 +45,172 @@ try {
 }
 
 ?>
-
-	<div id="multi-menu" class="col-md-3 hidden-sm hidden-xs transparent-menu">
-		<ul class="nav nav-pills nav-stacked">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
-
-	<div class="dropdown hidden-lg hidden-md" style="position:relative">
-		<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
-
-	<div class="container-fluid">
+<div class="container-fluid container-margin-sm user-account transparent-form" id="edit-product">
 	<div class="row">
-	<div class="col-sm-9 transparent-form">
-
-		<form class="form-inline transparent-form" id="editStoreController" method="post" action="../php/forms/edit-store-controller.php" enctype="multipart/form-data">
-			<h2>Edit Store</h2>
-			<?php echo generateInputTags(); ?>
-			<div class="form-group">
-				<label for="editStoreName">Store Name</label>
-				<input type="text" class="form-control" name="editStoreName" id="editStoreName" value="<?php echo $storeName;?>">
+		<div id="multi-menu" class="col-md-3 hidden-sm hidden-xs">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
+		<div class="dropdown hidden-lg hidden-md" style="position:relative">
+			<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-6">
+			<div class="hidden-xs hidden-sm center">
+				<h2>Edit Store</h2>
 			</div>
-			<br>
-			<div class="form-group">
-				<label for="editStoreDescription">Store Description</label>
-				<input type="text" class="form-control" name="editStoreDescription" id="editStoreDescription" value="<?php echo $storeDescription;?>">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="editInputImage">Image</label>
-				<input type="file" class="form-control hidden" name="editInputImage" id="editInputImage">
-			</div>
-			<br>
+			<form class="form-inline" id="editStoreController" method="post" action="../php/forms/edit-store-controller.php" enctype="multipart/form-data">
+				<?php echo generateInputTags(); ?>
+				<div class="hidden-lg hidden-md center">
+					<h2>Edit Store</h2>
+				</div>
+				<div class="form-group">
+					<label for="editStoreName">Store Name</label>
+					<input type="text" class="form-control" name="editStoreName" id="editStoreName" value="<?php echo $storeName;?>">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="editStoreDescription">Store Description</label>
+					<input type="text" class="form-control" name="editStoreDescription" id="editStoreDescription" value="<?php echo $storeDescription;?>">
+				</div>
+				<br>
+				<div class="form-group hidden">
+					<label for="editInputImage">Image</label>
+					<input type="file" class="form-control hidden" name="editInputImage" id="editInputImage">
+				</div>
+				<br>
+				<div class="form-group">
+					<input type="submit" class="form-control" id="editSubmit" name="editSubmit" value="Submit">
+				</div>
+				<br>
+				<p id="outputArea"></p>
+				<br>
+			</form>
+
+				<div class="form-group">
+					<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default addProductButton">Add Product</button>
+				</div>
+
+				<?php
+				$currentDir = dirname(__FILE__);
+				require_once ("../root-path.php");
+
+				// credentials
+				require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+				try {
+				// get the credentials information from the server and connect to the database
+					mysqli_report(MYSQLI_REPORT_STRICT);
+					$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
+					$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+
+					// grab all products by store id in dummy session
+					$products = Product::getAllProductsByStoreId($mysqli, $_SESSION['storeId']);
+					// create table of existing storeLocations
+					if($products !== null) {
+						echo '<div class=form-group>';
+						echo '<table class="table table-responsive">';
+						echo '<tr>';
+						echo '<th><h4>Products</h4></th>';
+						echo '<th></th>';
+						echo '</tr>';
+						foreach($products as $product) {
+							$productName = $product->getProductName();
+							$productId = $product->getProductId();
+							echo '<tr>';
+							echo '<td>'. $productName . '</td>';
+							echo '<td><button id="'.$productId.'" class="btn btn-default editProductButton">Edit</button></td>';
+							echo '<td><button id="'.$productId.'" class="btn btn-default deleteProductButton">Delete</button></td>';
+							echo '</tr>';
+						}
+						echo '</table>';
+						echo '</div>';
+					}
+
+				} catch(Exception $exception) {
+					echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+				}
+
+				?>
+
+				<br>
+				<div class="form-group">
+					<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default addButton">Add Pick-Up Location</button>
+				</div>
+
+				<?php
+				$currentDir = dirname(__FILE__);
+				require_once ("../root-path.php");
+
+				// credentials
+				require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+				try {
+					// get the credentials information from the server and connect to the database
+					mysqli_report(MYSQLI_REPORT_STRICT);
+					$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
+					$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+
+					// grab all storeLocations by store id in dummy session
+					$storeLocations = StoreLocation::getAllStoreLocationsByStoreId($mysqli, $_SESSION['storeId']);
+					// create table of existing storeLocations
+					if($storeLocations !== null) {
+
+						echo '<table class="table table-responsive">';
+						echo '<tr>';
+						echo '<th>Pick-Up Locations</th>';
+						echo '<th></th>';
+						echo '</tr>';
+						$locationCount = 0;
+						foreach($storeLocations as $storeLocation) {
+							$locationId = $storeLocation->getLocationId();
+							$location = Location::getLocationByLocationId($mysqli, $locationId);
+							$locationName = $location->getLocationName();
+							echo '<tr>';
+							echo '<td>'. $locationName . '</td>';
+							echo '<td><button id="'.$locationId.'" class="btn btn-default editButton">Edit</button></td>';
+							if($locationCount !== 0 ) {
+								echo '<td><button id="' . $locationId . '" class="btn btn-default deleteButton">Delete</button></td>';
+							}
+							$locationCount = 1;
+							echo '</tr>';
+						}
+						echo '</table>';
+					}
+
+				} catch(Exception $exception) {
+					echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+				}
+
+				?>
+
+				<br>
+				<div class="form-group">
+					<button class="btn btn-default addButton" id="back">Back</button>
+				</div>
+			</div><!-- col-sm-6 -->
 
 
-			<div class="form-group edit-product">
-				<a href="#" id="editStoreImageLink">
+
+
+			<div class="col-sm-3">
+				<div class="form-group mt25">
+						<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default linkStore">Link to store page</button>
+					</div>
+					<br>
+
+
+					<div class="form-group edit-product mt0">
+				<a href="#" id="editStoreImageLink" class="user-account">
 				<?php
 
 				$baseUrl             = CONTENT_ROOT_URL . 'images/store/';
@@ -101,24 +221,19 @@ try {
 				// show a placeholder if the product is not associated with an image
 				if(is_file($basePath . $imageSrc)) {
 					?>
-					<img class="thumbnail img-responsive" src="<?php echo $baseUrl . $imageSrc; ?>" alt="<?php echo $storeName; ?>"/>
+					<img class="thumbnail img-preview" src="<?php echo $baseUrl . $imageSrc; ?>" alt="<?php echo $storeName; ?>"/>
 				<?php } else { ?>
-					<img class="thumbnail img-responsive" src="<?php echo $imagePlaceholderSrc; ?>" alt="<?php echo $storeName; ?>"/>
+					<img class="thumbnail img-preview" src="<?php echo $imagePlaceholderSrc; ?>" alt="<?php echo $storeName; ?>"/>
 				<?php } ?>
 				</a>
+						</figure>
+						<figcaption>Change store image</figcaption>
 
-
+				</div>
 
 
 
 			</div>
-			<br>
-			<div class="form-group">
-					<input type="submit" class="form-control" id="editSubmit" name="editSubmit" value="Submit">
-			</div>
-			<br>
-			<p id="outputArea"></p>
-		</form>
 		<br>
 	</div><!-- col-sm-9 -->
 	</div><!-- row -->
@@ -130,122 +245,9 @@ try {
 			<div class="col-sm-9">
 
 
-						<div class="form-group">
-							<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default linkStore">Link to store page</button>
-						</div>
-						<br>
 
 
-						<div class="form-group">
-							<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default addProductButton">Add Product</button>
-						</div>
-
-						<?php
-
-
-						// dummy session
-						$currentDir = dirname(__FILE__);
-						require_once ("../root-path.php");
-
-						// credentials
-						require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
-						try {
-							// get the credentials information from the server and connect to the database
-							mysqli_report(MYSQLI_REPORT_STRICT);
-							$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
-							$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
-
-							// grab all storeLocations by store id in dummy session
-							$products = Product::getAllProductsByStoreId($mysqli, $_SESSION['storeId']);
-							// create table of existing storeLocations
-							if($products !== null) {
-
-								echo '<table class="table table-responsive">';
-								echo '<tr>';
-								echo '<th>Products</th>';
-								echo '<th></th>';
-								echo '</tr>';
-								foreach($products as $product) {
-									$productName = $product->getProductName();
-									$productId = $product->getProductId();
-									echo '<tr>';
-									echo '<td>'. $productName . '</td>';
-									echo '<td><button id="'.$productId.'" class="btn btn-default editProductButton">Edit</button></td>';
-									echo '<td><button id="'.$productId.'" class="btn btn-default deleteProductButton">Delete</button></td>';
-									echo '</tr>';
-								}
-								echo '</table>';
-							}
-
-						} catch(Exception $exception) {
-							echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
-						}
-
-						?>
-
-
-						<br>
-						<div class="form-group">
-							<button id="<?php echo $_SESSION['storeId'];?>" class="btn btn-default addButton">Add Pick-Up Location</button>
-						</div>
-
-						<?php
-
-
-						// dummy session
-						$currentDir = dirname(__FILE__);
-						require_once ("../root-path.php");
-
-						// credentials
-						require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
-						try {
-							// get the credentials information from the server and connect to the database
-							mysqli_report(MYSQLI_REPORT_STRICT);
-							$configArray = readConfig("/etc/apache2/capstone-mysql/farmtoyou.ini");
-							$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
-
-							// grab all storeLocations by store id in dummy session
-							$storeLocations = StoreLocation::getAllStoreLocationsByStoreId($mysqli, $_SESSION['storeId']);
-							// create table of existing storeLocations
-							if($storeLocations !== null) {
-
-								echo '<table class="table table-responsive">';
-								echo '<tr>';
-								echo '<th>Pick-Up Locations</th>';
-								echo '<th></th>';
-								echo '</tr>';
-								$locationCount = 0;
-								foreach($storeLocations as $storeLocation) {
-									$locationId = $storeLocation->getLocationId();
-									$location = Location::getLocationByLocationId($mysqli, $locationId);
-									$locationName = $location->getLocationName();
-									echo '<tr>';
-									echo '<td>'. $locationName . '</td>';
-									echo '<td><button id="'.$locationId.'" class="btn btn-default editButton">Edit</button></td>';
-									if($locationCount !== 0 ) {
-										echo '<td><button id="' . $locationId . '" class="btn btn-default deleteButton">Delete</button></td>';
-									}
-									$locationCount = 1;
-									echo '</tr>';
-								}
-								echo '</table>';
-							}
-
-						} catch(Exception $exception) {
-							echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
-						}
-
-						?>
-
-						<br>
-						<div class="form-group">
-							<button class="btn btn-default addButton" id="back">Back</button>
-						</div>
-					</div><!-- col-sm-6 -->
-		</div><!-- row -->
-	</div><!-- container-fluid -->
+<!-- container-fluid -->
 
 <!--js validation + ajax call-->
 <script src="../js/edit-store.js"></script>

bbb780dc133515a58ec89558ad34bbdf91d3eaa2
Alonso Indacochea <aindaco1@hotmail.com>
2015-03-08 17:00:06 -0600

    added location submit css rule and refined add location view

--- add-location/index.php
+++ add-location/index.php
@@ -21,62 +21,69 @@ require_once("../php/classes/location.php");
 
 ?>
 
-<div>
-	<div id="multi-menu" class="col-md-3 hidden-sm hidden-xs transparent-menu">
-		<ul class="nav nav-pills nav-stacked">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
+	<div class="container-fluid container-margin-sm transparent-form user-account">
+	<div class="row">
 
-	<div class="dropdown hidden-lg hidden-md" style="position:relative">
-		<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
-</div>
-<div class="row-fluid">
-	<div class="col-sm-9">
-		<form class="form-inline transparent-form" id="locationController" method="post" action="../php/forms/add-location-controller.php">
-			<h2>Add Location</h2>
+		<div id="multi-menu" class="col-md-3 hidden-sm hidden-xs">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
+
+		<div class="dropdown visible-xs" style="position:relative">
+			<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
+
+		<div class="col-sm-3 visible-xs">
+			<h2>Add Pick-Up Location</h2>
+		</div>
+		<div class="col-sm-6">
+		<form class="form-inline" id="locationController" method="post" action="../php/forms/add-location-controller.php">
 			<?php echo generateInputTags(); ?>
+			<div class="hidden-xs center">
+				<h2>Add Pick-Up Location</h2>
+			</div>
+
 			<div class="form-group">
 				<label for="locationName">Location Name</label>
-				<input type="text" id="locationName" name="locationName">
+				<input type="text" class="form-control" id="locationName" name="locationName">
 			</div>
 			<br>
 			<div class="form-group">
 				<label for="address1">Address</label>
-				<input type="text" id="address1" name="address1">
+				<input type="text" class="form-control" id="address1" name="address1">
 			</div>
 			<br>
 			<div class="form-group">
 				<label for="address2">Address Cont.</label>
-				<input type="text" id="address2" name="address2">
+				<input type="text" class="form-control" id="address2" name="address2">
 			</div>
 			<br>
 			<div class="form-group">
 				<label for="city">City</label>
-				<input type="text" id="city" name="city" value="Albuquerque">
+				<input type="text" class="form-control" id="city" name="city" value="">
 			</div>
 			<div class="form-group">
 				<label for="state">State</label>
-				<input type="text" id="state" name="state" value="NM">
+				<input type="text" class="form-control" id="state" name="state" value="">
 			</div>
 			<br>
 			<div class="form-group">
 				<label for="zipCode">Zip Code</label>
-				<input type="text" id="zipCode" name="zipCode" value="87048">
+				<input type="text" class="form-control" id="zipCode" name="zipCode" value="">
 			</div>
 			<div class="form-group">
 				<label for="country">Country</label>
-				<input type="text" id="country" name="country" value="US">
+				<input type="text" class="form-control" id="country" name="country" value="">
 			</div>
 			<br>
 			<br>
@@ -89,7 +96,7 @@ require_once("../php/classes/location.php");
 			<br>
 		</form>
 		<div class="form-group">
-			<button class="btn btn-default addButton" id="back">Back</button>
+			<button class="btn btn-default addLocationButton" id="back">Back</button>
 		</div>
 	</div>
 </div>
--- add-store/index.php
+++ add-store/index.php
@@ -25,96 +25,93 @@ $profileId = $_SESSION['profileId'];
 
 ?>
 
-	<div class="container-fluid container-margin-sm transparent-form user-account">
+<div class="container-fluid container-margin-sm transparent-form user-account">
 	<div class="row">
 
-	<div id="multi-menu" class="col-md-3 hidden-xs">
-		<ul class="nav nav-pills nav-stacked">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
-	<div class="dropdown visible-xs" style="position:relative">
-		<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="../edit-profile/index.php">Edit Profile</a></li>
-			<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
-			<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
-			<li><a href="../bank-account/index.php">Bank Account</a></li>
-		</ul>
-	</div>
-
+		<div id="multi-menu" class="col-md-3 hidden-xs">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
+		<div class="dropdown visible-xs" style="position:relative">
+			<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="../edit-profile/index.php">Edit Profile</a></li>
+				<li class="active"><a href="../add-store/index.php">Manage Stores</a></li>
+				<li><a href="../merchant-order-list/index.php">List of Orders</a></li>
+				<li><a href="../bank-account/index.php">Bank Account</a></li>
+			</ul>
+		</div>
 		<div class="col-sm-3 visible-xs">
 			<h2>Add Store</h2>
 		</div>
 		<div class="col-sm-6">
-		<form class="form-inline" id="storeController" method="post" action="../php/forms/add-store-controller.php" enctype="multipart/form-data">
-			<?php echo generateInputTags(); ?>
-			<div class="hidden-xs center">
-				<h2>Add Store</h2>
-			</div>
-			<div class="form-group">
-				<label for="storeName">Name</label>
-				<input type="text" class="form-control" id="storeName" name="storeName" value="">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="storeDescription">Description</label>
-				<textarea class="form-control" id="storeDescription" name="storeDescription"></textarea>
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="inputImage">Store Image</label>
-				<input type="file" class="form-control" id="inputImage" name="inputImage">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="locationName">Location Name</label>
-				<input type="text" class="form-control" id="locationName" name="locationName" value="">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="address1">Address</label>
-				<input type="text" class="form-control" id="address1" name="address1" value="">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="address2"></label>
-				<input type="text" class="form-control" id="address2" name="address2" value="">
-			</div>
-			<br>
-			<div class="form-group">
-				<label for="city">City</label>
-				<input type="text" class="form-control" id="city" name="city" value="">
-			</div>
-			<div class="form-group">
-				<label for="state">State</label>
-				<input type="text" class="form-control" id="state" name="state" value="">
-			</div>
-			<div class="form-group">
-				<label for="zipCode">Zip Code</label>
-				<input type="text" class="form-control" id="zipCode" name="zipCode" value="">
-			</div>
-			<div class="form-group">
-				<label for="country">Country</label>
-				<input type="text" class="form-control" id="country" name="country" value="">
-			</div>
-			<br>
-			<br>
-			<div class="form-group">
-				<input type="submit" class="form-control" id="editSubmit" action="../php/forms/store-edit-button-controller.php" name="editSubmit" value="Submit">
-			</div>
-			<br>
-			<br>
-			<p id="outputArea"></p>
-			<br>
-		</form>
+			<form class="form-inline" id="storeController" method="post" action="../php/forms/add-store-controller.php" enctype="multipart/form-data">
+				<?php echo generateInputTags(); ?>
+				<div class="hidden-xs center">
+					<h2>Add Store</h2>
+				</div>
+				<div class="form-group">
+					<label for="storeName">Name</label>
+					<input type="text" class="form-control" id="storeName" name="storeName" value="">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="storeDescription">Description</label>
+					<textarea class="form-control" id="storeDescription" name="storeDescription"></textarea>
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="inputImage">Store Image</label>
+					<input type="file" class="form-control" id="inputImage" name="inputImage">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="locationName">Location Name</label>
+					<input type="text" class="form-control" id="locationName" name="locationName" value="">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="address1">Address</label>
+					<input type="text" class="form-control" id="address1" name="address1" value="">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="address2"></label>
+					<input type="text" class="form-control" id="address2" name="address2" value="">
+				</div>
+				<br>
+				<div class="form-group">
+					<label for="city">City</label>
+					<input type="text" class="form-control" id="city" name="city" value="">
+				</div>
+				<div class="form-group">
+					<label for="state">State</label>
+					<input type="text" class="form-control" id="state" name="state" value="">
+				</div>
+				<div class="form-group">
+					<label for="zipCode">Zip Code</label>
+					<input type="text" class="form-control" id="zipCode" name="zipCode" value="">
+				</div>
+				<div class="form-group">
+					<label for="country">Country</label>
+					<input type="text" class="form-control" id="country" name="country" value="">
+				</div>
+				<br>
+				<br>
+				<div class="form-group">
+					<input type="submit" class="form-control" id="editSubmit" action="../php/forms/store-edit-button-controller.php" name="editSubmit" value="Submit">
+				</div>
+				<br>
+				<br>
+				<p id="outputArea"></p>
+				<br>
+			</form>
 <?php
 
-
-// dummy session
 $currentDir = dirname(__FILE__);
 require_once ("../root-path.php");
 
@@ -157,9 +154,9 @@ try {
 }
 
 ?>
-			</div>
 		</div>
 	</div>
+</div>
 
 <!--js validation + ajax call-->
 <script src="../js/add-store.js"></script>

8c969eb7adbf9cfd523807f6c5c9a43cc218e574
Jay Renteria <jrenteria13@cnm.edu>
2015-03-10 17:09:05 -0600

    changed button position for mobile

--- new-user/index.php
+++ new-user/index.php
@@ -27,14 +27,14 @@ require_once('../php/lib/header.php');
 		<div class="row">
 			<div class="col-xs-12">
 				<h1 class="heading hidden-xs">Delicious products and fair trades directly from the farmers</h1>
-				<form class="mt30" action="../php/forms/search-controller.php" id="search" method="post">
+				<form  action="../php/forms/search-controller.php" id="search" method="post">
 					<?php echo generateInputTags(); ?>
 					<div class="input-group">
 						<input class="form-control search-field-splash" type="text" id="inputSearch" name="inputSearch" placeholder="What are you looking for today?" />
 						<input type="hidden" value="yes" name="searching">
-						<span class="input-group-btn">
-						  <button id="inputSubmit" class="btn btn-primary search-field-splash" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-						</span>
+<!--						<span class="">-->
+						  <button id="inputSubmit" class="btn btn-primary search-field-button" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+<!--						</span>-->
 					</div>
 				</form>
 				<p class="outputArea"></p>

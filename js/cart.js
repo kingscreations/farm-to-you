66ae89ef79ac5bade2563afd422070bd8f333345
flogo1982 <florian.goussin@gmail.com>
2015-03-11 16:06:41 -0600

    put the mobile code apart to debug it

--- js/cart.js
+++ js/cart.js
@@ -34,6 +34,7 @@
 		 $productQuantity = typeof $current === 'undefined' ? $(this) : $current;
 
 		 // first step to be able to get the other cell of this product row
+		 // #product quantity ID
 		 var elementId = $productQuantity[0].id;
 
 		 // get the first part of the id: product# which gives the product number (the row)
@@ -115,4 +116,60 @@
 				 location.reload();
 			 });
 	 });
+
+
+	 // mobile xp
+
+	 //$productQuantity = $('[id^=product-mobile-]')
+	 //
+	 //$.each($productQuantity, function() {
+		// refreshTotalProductPrice($(this));
+	 //});
+	 //
+	 ///**
+	 // * refresh the total product price of a product
+	 // */
+	 //function refreshTotalProductPriceForMobile($current) {
+	 //
+		// // #product quantity ID
+		// var elementId = $productQuantity[0].id;
+	 //
+		// // get the first part of the id: product# which gives the product number (the row)
+		// var elementIdPart1 = elementId.split('-')[0];
+	 //
+		// var $inputPrice = $('#accordion #');
+		// var $inputWeight = $('#'+ elementIdPart1 +'-weight');
+	 //
+		// // get the product weight and the new quantity
+		// var productWeight = parseFloat($inputWeight.text());
+		// var newQuantity = parseFloat($productQuantity.val());
+		// var productPrice = $inputPrice.text();
+	 //
+		// // set the total price according to the productPriceType
+		// if($inputPrice.text().indexOf('lb') >= 0) {
+	 //
+		//	 // get rid of the /lb AND the $ (first letter)
+		//	 productPrice = parseFloat(productPrice.split('/lb')[0].substring(1));
+	 //
+		//	 // price per pound
+		//	 var result = productPrice * newQuantity * productWeight;
+	 //
+		//	 // multiply by 100, round and then divide by 100 to get 2 decimal precision
+		//	 var totalProductPrice = String((Math.round(result * 100) / 100).toFixed(2));
+	 //
+		//	 $('#'+ elementIdPart1 +'-final-price').html('$'+totalProductPrice);
+		// } else {
+	 //
+		//	 // get just rid of the $ (first letter)
+		//	 productPrice = parseFloat(productPrice.substring(1));
+	 //
+		//	 // unit price
+		//	 var result = productPrice * newQuantity;
+	 //
+		//	 // multiply by 100, round and then divide by 100 to get 2 decimal precision
+		//	 var totalProductPrice = String((Math.round(result * 100) / 100).toFixed(2));
+	 //
+		//	 $('#'+ elementIdPart1 +'-final-price').html('$'+totalProductPrice);
+		//};
+	 //}
 });

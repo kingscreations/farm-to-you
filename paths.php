859206e5fc316a3446bf5ba2cb63264fc21d38cf
flogo1982 <florian.goussin@gmail.com>
2015-03-12 16:21:59 -0600

    update the paths

--- paths.php
+++ paths.php
@@ -1,7 +1,7 @@
 <?php
 
 // content path
-define('CONTENT_ROOT_URL', 'https://bootcamp-coders.cnm.edu/farm-to-you/');
+define('CONTENT_ROOT_URL', 'https://farmtoyou.me/');
 define('CONTENT_ROOT_PATH', '/var/www/html/farm-to-you/');
 
 define('IMAGE_PATH', 'images/');

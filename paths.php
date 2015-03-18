0177068901ea8c64fde820aef20b7852753d08c7
flogo1982 <florian.goussin@gmail.com>
2015-03-18 09:43:58 -0600

    dynamic root path: needs to be tested on the main site url

--- paths.php
+++ paths.php
@@ -6,16 +6,12 @@ define('CONTENT_ROOT_PATH', '/var/www/html/farm-to-you/');
 
 define('IMAGE_PATH', 'images/');
 
-// get the root url and define a constant from it
-//$currentPathExploded = explode("/", $_SERVER["PHP_SELF"]);
-//if(empty($currentPathExploded)) {
-//	throw new RangeException('Impossible to explode the path');
-//}
+//$rootUrl = 'http://farmtoyou.me/';
+//
+//define('SITE_ROOT_URL', $rootUrl);
+//define('SITE_ROOT_PATH', dirname(__FILE__).'/');
 
-//$rootUrl = "http://". $_SERVER["SERVER_NAME"] . '/' . $currentPathExploded[1] . '/' .
-//	$currentPathExploded[2] . '/';
-
-$rootUrl = 'http://farmtoyou.me/';
+$rootUrl = "http://". $_SERVER["SERVER_NAME"] . '/';
 
 define('SITE_ROOT_URL', $rootUrl);
 define('SITE_ROOT_PATH', dirname(__FILE__).'/');
